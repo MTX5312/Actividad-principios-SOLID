f682255a1dffebcbd6b3f46ac8b804b950d25135
Tomás Méndez <tomasmendez@outlook.com>
2025-09-23 22:49:59 -0300

    Codigo Inversion de Dependencias
    En este codigo se utiliza el principio de inversion de dependencias

--- Codigo.cs
+++ Codigo.cs
@@ -1,9 +1,51 @@
 ﻿using System;
 
-public class Class1
+class Program
 {
-	public Class1()
+	static void Main(string[] args)
 	{
+		Objeto gravitador = new CambiaGravedad();
+		Objeto gancho = new Gancho();
+		PJ corredor = new PJ(gancho);
+		corredor.UsarObjeto();
+    }
+}
 
+public class PJ
+{
+	private Objeto objeto;
+	
+	public PJ(Objeto objeto)
+	{
+		this.objeto = objeto;
+    }
+
+	public void UsarObjeto()
+	{
+		objeto.usar();
 	}
 }
+
+public interface Objeto
+{
+	void usar();
+}
+
+public class CambiaGravedad : Objeto
+{
+	public void usar()
+	{
+		Console.WriteLine("Cambiando la gravedad...");
+    }
+}
+
+public class Gancho : Objeto
+{
+	public void usar()
+	{
+		Console.WriteLine("Usando el gancho...");
+	}
+}
+
+//Como la clase depende de una abstraccion y no una implementacion concreta,
+//podemos cambiar o añadir nuevos objetos sin modificar al jugador
