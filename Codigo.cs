eef55946e86aa839d987c085f38d40f863504268
Tomás Méndez <tomasmendez@outlook.com>
2025-09-23 22:35:59 -0300

    Codigo respetando el principio

--- Codigo.cs
+++ Codigo.cs
@@ -1,9 +1,46 @@
 ﻿using System;
 
-public class Class1
+public class Enemigo
 {
-	public Class1()
+	public string Nombre;
+	public int Vida;
+	
+	public Enemigo(string nombre, int vida)
 	{
+		Nombre = nombre;
 
-	}
+		Vida = vida;
+    }
+
+	public void RecibirDanio(int danio)
+		{
+		Vida -= danio;
+		if (Vida < 0)
+		{
+			Vida = 0;
+		}
+		Console.WriteLine($"{Nombre} ha recibido {danio} de daño. Vida restante: {Vida}");
+    }
 }
+
+public class GenerarEnemigos()
+{
+	public void Generar(Enemigo fantasma)
+	{
+		Console.WriteLine($"Generando {fantasma.name} en la pantalla...");
+    }
+}
+
+public class JuegoTerminado()
+{
+	public void VerificarDerrota(Enemigo enemigo)
+	{
+		if (enemigo.Vida <= 0)
+		{
+			Console.WriteLine($"{enemigo.Nombre} ha sido derrotado!");
+		}
+    }
+}
+
+//Las responsabilidades se dividen entre las clases
+//Se pueden realizar modificaciones sin cambiar la clase Enemigo
